d6beb5b035cc1aabb0670150c71e0131e7a44eb4
Jose Tiago Macara Coutinho <tcoutinho@cells.es>
2021-03-12 18:10:58 +0100

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sardana-iba",
+    version = "0.0.1",
+    description = "IBA Sardana Controller",
+    author = "ALBA",
+    author_email = "controls@cells.es",
+    license = "GPLv3",
+    url = "https://github.com/ALBA-Synchrotron/sardana-iba",
+    packages = find_packages(),
+    install_requires = ['sardana'],
+)
