23bdff9633b39deaa6bf92418eeb46b0611b661f
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-05-11 10:25:14 +0200

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sardana-iba",
-    version = "0.0.1",
+    version = "version="0.1.0"",
     description = "IBA Sardana Controller",
     author = "ALBA",
     author_email = "controls@cells.es",
